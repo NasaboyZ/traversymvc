e200c79f9aac80b6c4eeb827d2b59594cc93ad1d
Josef Leite <josef@MBP-von-Josef.fritz.box>
2024-06-17 23:41:26 +0200

    Styling Validation ist nun on point

--- app/views/admin/regristrationsformular.php
+++ app/views/admin/regristrationsformular.php
@@ -7,7 +7,7 @@
     </div>
 
     <header class="header">
-      <div class="header__avatar">Welcome <?php echo isset($data['username']) ? htmlspecialchars($data['username']) : 'Guest'; ?>!</div>
+    <div class="header__avatar">Welcome <?php echo htmlspecialchars($data['username']); ?>!</div>
     </header>
 
     <aside class="sidenav">
@@ -48,11 +48,11 @@
     </aside>
 
     <main class="main">
-      <div class="main-cards">
-        <div class="card">
+      <div class="main-cards-register">
+        <div class="card-register">
           <h2>Admin Registration</h2>
-          <form action="<?php echo URLROOT; ?>/admin/registerAdmin" method="POST">
-            <div class="form-group">
+          <form class="register-form" action="<?php echo URLROOT; ?>/admin/registerAdmin" method="POST">
+            <div class="form-group-register">
                 <label for="anrede">Anrede: <sup>*</sup></label>
                 <select name="anrede" class="form-control">
                     <option value="Herr" <?php echo isset($data['anrede']) && $data['anrede'] == 'Herr' ? 'selected' : ''; ?>>Herr</option>
@@ -60,41 +60,41 @@
                 </select>
                 <span class="invalid-feedback"><?php echo isset($data['errors']['anrede']) ? $data['errors']['anrede'] : ''; ?></span>
             </div>
-            <div class="form-group">
+            <div class="form-group-register">
                 <label for="firstname">Vorname: <sup>*</sup></label>
                 <input type="text" name="firstname" class="form-control" value="<?php echo isset($data['firstname']) ? $data['firstname'] : ''; ?>">
                 <span class="invalid-feedback"><?php echo isset($data['errors']['firstname']) ? $data['errors']['firstname'] : ''; ?></span>
             </div>
-            <div class="form-group">
+            <div class="form-group-register">
                 <label for="secondName">Nachname: <sup>*</sup></label>
                 <input type="text" name="secondName" class="form-control" value="<?php echo isset($data['secondName']) ? $data['secondName'] : ''; ?>">
                 <span class="invalid-feedback"><?php echo isset($data['errors']['secondName']) ? $data['errors']['secondName'] : ''; ?></span>
             </div>
-            <div class="form-group">
+            <div class="form-group-register">
                 <label for="nutzername">Benutzername: <sup>*</sup></label>
                 <input type="text" name="nutzername" class="form-control" value="<?php echo isset($data['nutzername']) ? $data['nutzername'] : ''; ?>">
                 <span class="invalid-feedback"><?php echo isset($data['errors']['nutzername']) ? $data['errors']['nutzername'] : ''; ?></span>
             </div>
-            <div class="form-group">
+            <div class="form-group-register">
                 <label for="email">E-Mail: <sup>*</sup></label>
                 <input type="email" name="email" class="form-control" value="<?php echo isset($data['email']) ? $data['email'] : ''; ?>">
                 <span class="invalid-feedback"><?php echo isset($data['errors']['email']) ? $data['errors']['email'] : ''; ?></span>
             </div>
-            <div class="form-group">
+            <div class="form-group-register">
                 <label for="password">Passwort: <sup>*</sup></label>
                 <input type="password" name="password" class="form-control" value="<?php echo isset($data['password']) ? $data['password'] : ''; ?>">
                 <span class="invalid-feedback"><?php echo isset($data['errors']['password']) ? $data['errors']['password'] : ''; ?></span>
             </div>
-            <div class="form-group">
+            <div class="form-group-register">
                 <label for="password_repeat">Passwort wiederholen: <sup>*</sup></label>
                 <input type="password" name="password_repeat" class="form-control" value="<?php echo isset($data['password_repeat']) ? $data['password_repeat'] : ''; ?>">
                 <span class="invalid-feedback"><?php echo isset($data['errors']['password_repeat']) ? $data['errors']['password_repeat'] : ''; ?></span>
             </div>
-            <div class="form-group">
+            <div class="form-group-register">
                 <label for="agb">AGB: <sup>*</sup></label>
                 <input type="checkbox" name="agb" value="1" <?php echo isset($data['agb']) && $data['agb'] == 1 ? 'checked' : ''; ?>> Ich akzeptiere die AGB
-                <span class="invalid-feedback"><?php echo isset($data['errors']['agb']) ? $data['errors']['agb'] : ''; ?></span>
             </div>
+            <span class="invalid-feedback"><?php echo isset($data['errors']['agb']) ? $data['errors']['agb'] : ''; ?></span>
             <input type="submit" class="btn" value="Regristieren">
           </form>
         </div>
